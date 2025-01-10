26d323d250bdcf63197d1d38588ebc03e4e85cfb
Miguel Dias <migueladp33@gmail.com>
2025-01-10 17:16:22 -0300

    My First Contract in Solidity
    My first contract in solidity and node scripts for web3

---
+++ SECTION 1/contracts/novoContrato.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FirstContract {
+
+    uint256 public myNumber =10;
+    string public  __init__Btc = "Keys Block ****/api/id:four/jsonweb3?number=4"; 
+    
+}
+    
